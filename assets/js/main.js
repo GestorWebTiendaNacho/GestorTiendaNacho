4ef8fa08cd51649e093dac43e6171092c713ad66
GestorTiendaNacho <cn.apistock@gmail.com>
2026-05-27 19:19:06 -0300

    Nico actualiza

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
     }
     window.mainJsCargado = true;
 
-    window.URL_GAS_GLOBAL = "https://script.google.com/macros/s/AKfycbznIQIL3lt7nyILMztd8hgIvbhmeE4ibaTH0vSoxQACcbLqX1snkjthgUQXZ4QT9S8/exec";
+    window.URL_GAS_GLOBAL = "https://script.google.com/macros/s/AKfycbzCWh6sLeLMdGw6EyMPS-WmvDpctm7EWUSy96Xc9H4Ldkz0ewUYh9ZAGEUrVhLgBFE/exec";
     /**
      * Función global para comunicarse con Google Apps Script
      * @param {string} accion - El nombre de la acción a ejecutar (sync_stock, get_progress, etc)
